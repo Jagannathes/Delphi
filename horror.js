791a841fd9513c3533cb4e204ab6d3437748ee46
Jagannathes <75128235+Jagannathes@users.noreply.github.com>
2021-06-17 11:36:50 +0530

    Update horror.js

--- horror.js
+++ horror.js
@@ -3,7 +3,7 @@ async function hehe() {
     var k = document.getElementById("con");
     
     
-      var data = await fetch("https://www.googleapis.com/books/v1/volumes?q=subject:Fantasy");
+      var data = await fetch("https://www.googleapis.com/books/v1/volumes?q=subject:thriller");
       const response =  await data.json();
    for (var i = 0; i < response.items.length; i+=1) {
    
